fd760a48bf8656b5e116868d05650c619335a411
Matheus Souza <matheu.sk.9@hotmail.com>
2020-11-24 16:30:24 -0300

    Create routes structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 
-function App() {
-  return <div className="App" />;
-}
+import { SignIn, SignUp, Home } from './components/pages';
+
+const App = () => {
+  const user = null;
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/sign-up">
+          <SignUp />
+        </Route>
+        <Route path="/sign-in">
+          <SignIn />
+        </Route>
+        <Route
+          path="/"
+          render={({ location }) =>
+            user ? (
+              <Home />
+            ) : (
+              <Redirect
+                to={{ pathname: '/sign-in', state: { from: location } }}
+              />
+            )
+          }
+        />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
